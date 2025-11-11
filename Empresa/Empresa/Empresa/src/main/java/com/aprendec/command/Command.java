ee8b57d733c35d78b33bb98876058be6f6ce5e31
GGarno0 <cristianestevesrosas@gmail.com>
2025-11-11 09:20:04 +0100

    Delete Empresa directory

--- Empresa/Empresa/Empresa/src/main/java/com/aprendec/command/Command.java
+++
@@ -1,8 +0,0 @@
-package com.aprendec.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Command {
-    void ejecutar(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
